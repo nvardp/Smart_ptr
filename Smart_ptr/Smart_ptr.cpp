74f4db6763bb6c667ed85a12a6c6ed97c252a181
nvardp21 <nvardp21@gmail.com>
2021-04-27 17:59:03 +0400

    Add project files.

---
+++ Smart_ptr/Smart_ptr.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+template <typename T>
+class shared_ptr
+{
+public:
+explicit shared_ptr(T*)
+	:m_counter(0)
+{ 
+	m_ptr = T;
+	m_counter++;
+	}
+
+	~shared_ptr() 
+	{
+		m_counter--;
+		if(m_counter ==0)
+		delete m_ptr;
+	}
+
+	shared_ptr(const shared_ptr<T>& rhs)
+	{
+		m_ptr = rhs.m_ptr;
+		m_counter = rhs.m_counter;
+		m_counter++;
+	}
+	shared_ptr&	operator=(const shared_ptr<T>& rhs)
+	{
+		m_ptr = rhs.m_ptr;
+		m_counter = rhs.m_counter;
+		m_counter++;
+		return *this;
+	}
+	T& operator*()
+	{
+		return *m_ptr;
+	}
+	T* operator->()
+	{
+		return m_ptr;
+	}
+	
+private:
+	shared_ptr<T>* m_ptr;
+volatile static	unsigned int m_counter;
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+   
+}
+
